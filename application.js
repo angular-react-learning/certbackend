59f88587d7e5ac0a4ee266054630ce74104b7ed8
Praveen <praveen@example.com>
2020-04-21 21:33:52 +0530

    added headers.

--- application.js
+++ application.js
@@ -8,5 +8,11 @@ application.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 application.use(bodyParser.json())
 
+application.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 application.use("/users", UserRoutes)
 module.exports = application;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const application = require("./application");
 const db = require("./db");
-application.listen(3003, ()=>{
-    console.log("Application started at 3003")
+application.listen(3005, ()=>{
+    console.log("Application started at 3005")
 })
